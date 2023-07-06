accb9ff0601965c911b3d52fccd7b509a0239fbf
wwwaas0 <wwwaas0@ajou.ac.kr>
2023-07-06 23:19:05 +0900

    수정 파일이 커밋안된 거 같아서 다시 커밋함(jwt 완료)

--- src/main/java/rabbit/umc/com/demo/user/UserService.java
+++ src/main/java/rabbit/umc/com/demo/user/UserService.java
@@ -1,2 +1,28 @@
-package rabbit.umc.com.demo.user;public class UserService {
+package rabbit.umc.com.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import rabbit.umc.com.config.BaseException;
+import rabbit.umc.com.config.BaseResponseStatus;
+import rabbit.umc.com.demo.user.Domain.User;
+import rabbit.umc.com.demo.user.Dto.UserEmailNicknameReqDto;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public void getEmailandNickname(UserEmailNicknameReqDto userEmailNicknameReqDto) throws BaseException {
+        //해당 유저 아이디 먼저 찾고
+        Optional<User> optionalUser = userRepository.findById(userEmailNicknameReqDto.getId());
+        User user = optionalUser.orElseThrow(() -> new BaseException(BaseResponseStatus.RESPONSE_ERROR));
+        user.setUserName(userEmailNicknameReqDto.getUserName());
+        user.setUserEmail(userEmailNicknameReqDto.getUserEmail());
+        //해당 유저 엔티티에 나머지값 업데이트
+        userRepository.save(user);
+    }
 }
