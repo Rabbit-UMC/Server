5e27c46c8f11f5a4d59db4cef81c6b10c457c081
leedongha98 <eedongha@naver.com>
2023-12-31 19:41:24 +0900

    api 기능 추가

--- src/main/java/rabbit/umc/com/demo/schedule/repository/ScheduleRepository.java
+++ src/main/java/rabbit/umc/com/demo/schedule/repository/ScheduleRepository.java
@@ -1,18 +1,14 @@
 package rabbit.umc.com.demo.schedule.repository;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 import rabbit.umc.com.demo.schedule.domain.Schedule;
 
 import java.sql.Timestamp;
-import java.time.Month;
-import java.time.Year;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -38,4 +34,6 @@ public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
 //    @Query("select s from Schedule s where month(s.endAt) = :month and s.user.id = :userId order by s.endAt asc")
     @Query("select s from Schedule s where s.user.id= :userId and year(s.endAt) = :year and month(s.endAt) = :month order by  s.endAt asc")
     List<Schedule> findSchedulesByMonth(@Param(value = "month") int month, @Param(value = "userId") Long userId, @Param(value = "year") int year);
+
+    Integer countByEndAtIs(LocalDateTime endAt);
 }
