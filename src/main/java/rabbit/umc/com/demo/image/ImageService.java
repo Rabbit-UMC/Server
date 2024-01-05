7377b22597b8f172b45e00d98c063a44c5db8d02
hosung-222 <gik11kr@naver.com>
2024-01-05 16:03:36 +0900

    :recycle: [refactor] : Image 업로드 API 파일 이동 (#176)

--- src/main/java/rabbit/umc/com/demo/image/ImageService.java
+++ src/main/java/rabbit/umc/com/demo/image/ImageService.java
@@ -1,4 +1,14 @@
 package rabbit.umc.com.demo.image;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ImageService {
+    private final ImageRepository imageRepository;
+
+
 }
