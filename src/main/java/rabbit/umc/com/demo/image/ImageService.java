9cd9568a1ea0a7f0575edfaf34e57b9732af52a3
hosung-222 <gik11kr@naver.com>
2024-01-23 21:40:54 +0900

    :sparkles: [feat] : MultipartFile 을 업로드 메서드 구현 (#199)

--- src/main/java/rabbit/umc/com/demo/image/ImageService.java
+++ src/main/java/rabbit/umc/com/demo/image/ImageService.java
@@ -1,20 +1,37 @@
 package rabbit.umc.com.demo.image;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import rabbit.umc.com.demo.community.domain.Article;
 import rabbit.umc.com.demo.community.dto.PatchArticleReq.ChangeImageDto;
 import rabbit.umc.com.demo.converter.ImageConverter;
+import rabbit.umc.com.utils.S3Uploader;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
+    private final S3Uploader s3Uploader;
+
+    @Transactional
+    public List<String> getImageUrl(List<MultipartFile> multipartFiles, String path) throws IOException {
+        List<String> filePathList = new ArrayList<>();
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            String filePath = s3Uploader.upload(multipartFile, path );
+            filePathList.add(filePath);
+        }
+
+        return filePathList;
+    }
 
     public List<Image> getArticleImages(Long articleId){
         return imageRepository.findAllByArticleId(articleId);
