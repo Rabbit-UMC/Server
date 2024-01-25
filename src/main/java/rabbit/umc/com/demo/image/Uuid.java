7fb63fce384c6b26ea8543b74df3075e6855800d
hosung-222 <gik11kr@naver.com>
2024-01-25 18:23:57 +0900

    :sparkles: [feat] : Uuid 엔티티 구현 (#212)

---
+++ src/main/java/rabbit/umc/com/demo/image/Uuid.java
@@ -0,0 +1,27 @@
+package rabbit.umc.com.demo.image;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rabbit.umc.com.config.BaseTimeEntity;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Uuid extends BaseTimeEntity {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+}
