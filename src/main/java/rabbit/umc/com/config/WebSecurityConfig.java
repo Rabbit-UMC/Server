713a95f32c5d86a82470fe078e426d200a4acc31
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-02-11 01:09:02 +0900

    feat: configure 작성 - 권한 설정(#222)

--- src/main/java/rabbit/umc/com/config/WebSecurityConfig.java
+++ src/main/java/rabbit/umc/com/config/WebSecurityConfig.java
@@ -4,20 +4,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                // Spring Boot에서 설정한 CORS (Cross-Origin Resource Sharing) 설정을 따른다 (config/WebConfig.java 설정 확인)
+//                .cors()
+//                .and()
+//                // CSRF (Cross-Site Request Forgery) 방지를 해제한다
+//                // Session/Cookie의 취약점을 이용하기 때문에 템플릿처럼 OAuth2, JWT 기반일 경우는 비활성화해도 무관하다.
+//                .csrf().disable()
+//                .build();
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+
                 // Spring Boot에서 설정한 CORS (Cross-Origin Resource Sharing) 설정을 따른다 (config/WebConfig.java 설정 확인)
                 .cors()
                 .and()
                 // CSRF (Cross-Site Request Forgery) 방지를 해제한다
                 // Session/Cookie의 취약점을 이용하기 때문에 템플릿처럼 OAuth2, JWT 기반일 경우는 비활성화해도 무관하다.
                 .csrf().disable()
-                .build();
+
+                .authorizeRequests()
+                // login 없이 허용
+                .antMatchers("/app/users/kakao-login").permitAll()
+                .antMatchers("/app/users/sign-up").permitAll()
+                .antMatchers("/app/users/checkDuplication").permitAll()
+
+                .antMatchers("/app/admin/**").hasRole("ADMIN")
+                .antMatchers("/app/host/**").hasRole("HOST")
+
+                // 그 외 어떤 요청이든 '인증'과정 필요
+                .anyRequest().authenticated();
+
     }
 }
