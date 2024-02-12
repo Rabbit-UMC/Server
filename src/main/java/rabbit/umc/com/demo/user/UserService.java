065658ad929f2517f09f78cec88e1976134fcc37
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-02-13 02:30:41 +0900

    fix: LazyInitializationException 해결 (#222)

--- src/main/java/rabbit/umc/com/demo/user/UserService.java
+++ src/main/java/rabbit/umc/com/demo/user/UserService.java
@@ -366,12 +366,14 @@ public class UserService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
 
         User user = userRepository.getReferenceById(new Long(userId));
         if (user == null) {
             new UsernameNotFoundException("회원번호 " + userId + " 님을 찾을 수 없습니다.");
         }
+        System.out.println("유저 롤: "+user.getUserPermission());
 
         return new UserDetailsImpl(user);
     }
