795df5aa70c6db638afce77646be254fc5d4f935
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-02-13 22:10:04 +0900

    refactor: 권한 변경 확인하도록 수정 (#222)

--- src/main/java/rabbit/umc/com/config/JwtAuthenticateFilter.java
+++ src/main/java/rabbit/umc/com/config/JwtAuthenticateFilter.java
@@ -3,6 +3,8 @@ package rabbit.umc.com.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -15,8 +17,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.Collection;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,21 +38,18 @@ public class JwtAuthenticateFilter extends OncePerRequestFilter {
             userId = String.valueOf(userIdx);
 
             // 현재 SecurityContextHolder 에 인증객체가 있는지 확인
-            System.out.println("user id: "+userId);
-            System.out.println("인증객체: "+SecurityContextHolder.getContext().getAuthentication());
-            if (userId != "0" /*&& SecurityContextHolder.getContext().getAuthentication() == null*/) {
-                UserDetails userDetails = userService.loadUserByUsername(userId);
+            Authentication existingAuth = SecurityContextHolder.getContext().getAuthentication();
+            UserDetails userDetails = userService.loadUserByUsername(userId);
+            Collection<? extends GrantedAuthority> userDetailsAuthorities = userDetails.getAuthorities();
 
-                // 토큰 유효여부 확인
-                log.info("JWT Filter token = {}", token);
-                log.info("JWT Filter userDetails = {}", userDetails.getUsername());
-                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
-                            = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            if (userId != "0" && existingAuth == null || !existingAuth.getAuthorities().equals(userDetailsAuthorities)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                        = new UsernamePasswordAuthenticationToken(userDetails, null, userDetailsAuthorities);
 
-                    usernamePasswordAuthenticationToken
-                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
             filterChain.doFilter(request, response);
 
